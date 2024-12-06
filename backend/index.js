0062ab8e1c76daf65b3b975711253afeef383313
Syed Mughis Hussain <syedmughis4541@gmail.com>
2024-12-06 13:36:24 +0500

    Cors aded in backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import connectDb from "./config/connectDb.js";
 
@@ -11,6 +12,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
