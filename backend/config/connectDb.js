fa163982cbae925f1fea4cb5f7992a1e7e33a387
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-12-07 12:22:46 +0500

    Changed made in connectDb function

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        const response = await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log('Connected to Database Successfully');
     } catch (error) {
         res.status(400).json({
