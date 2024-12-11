94583978f3110ca4af0c14ff5e339bc6597995e5
Syed Mughis Hussain <syedmughis4541@gmail.com>
2024-12-11 20:47:30 +0500

    Changes made

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -77,10 +77,6 @@ export default function Dashboard() {
   const [open, setOpen] = React.useState(false);
   const [loading, setLoading] = useState(true); // State for loading
 
-  if (!localStorage.getItem("token")) {
-    navigate("/");
-  }
-
   const handleDrawerOpen = () => {
     setOpen(true);
   };
